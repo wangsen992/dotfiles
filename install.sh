1a67b68a8e2328316ebde300746e7137eed07327
Sen Wang <wangsen992@gmail.com>
2025-11-10 14:19:55 -0600

    use git install rather than tpm for catppuccin

--- install.sh
+++ install.sh
@@ -6,4 +6,6 @@ ln -s $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
 
 # neovim setup
 mkdir -p $HOME/.config
+mkdir -p ~/.tmux/plugins/catppuccin
+git clone -b v2.1.3 https://github.com/catppuccin/tmux.git ~/.tmux/plugins/catppuccin/tmux
 ln -s $HOME/dotfiles/nvim $HOME/.config/nvim
