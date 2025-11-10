fbe140d1a8522446135d432fb274eeb6984b8476
Sen Wang <wangsen992@gmail.com>
2025-11-10 12:46:49 -0600

    correct sudo compatibility

--- scripts/install_nvim.sh
+++ scripts/install_nvim.sh
@@ -3,8 +3,9 @@
 # Create User Env
 # echo USER: $USER
 # export USER_HOME=/home/${USER}
+SUDO=$(which sudo)
 
-wget install-node.vercel.app/lts && sudo bash lts --yes && rm lts
+wget install-node.vercel.app/lts && $SUDO bash lts --yes && rm lts
 
 # download nvim source code and install pre-requistes
 # cd /app
@@ -13,7 +14,7 @@ tar xzvf v0.11.5.tar.gz
 rm v0.11.5.tar.gz
 
 cd neovim-0.11.5
-sudo apt-get install -y ninja-build gettext cmake unzip curl build-essential
+$SUDO apt-get install -y ninja-build gettext cmake unzip curl build-essential
 # make CMAKE_BUILD_TYPE=Release
-sudo make CMAKE_INSTALL_PREFIX=/usr/local install
-sudo rm -rf neovim-stable
+$SUDO make CMAKE_INSTALL_PREFIX=/usr/local install
+$SUDO rm -rf neovim-stable
