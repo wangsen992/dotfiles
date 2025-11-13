a542749e2fa83ac3da51c07ab0b1efe0d8bd43d4
Sen Wang <wangsen992@gmail.com>
2025-11-12 23:08:49 -0500

    rearrange homebrew install

--- install.sh
+++ install.sh
@@ -17,6 +17,14 @@ $SUDO update-locale LANG=en_US.UTF-8
 echo "export LANG=en_US.UTF-8"  >> ~/.zshrc
 echo "export LC_ALL=en_US.UTF-8" >> ~/.zshrc
 
+# homebrew setup
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";\ 
+  echo >> /home/${USER}/.zshrc; \
+  echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/user/.zshrc; \
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+/home/linuxbrew/.linuxbrew/bin/brew install neovim node
+
 # tmux setup
 $SUDO apt install -y tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
@@ -27,11 +35,3 @@ mkdir -p $HOME/.config
 mkdir -p ~/.tmux/plugins/catppuccin
 git clone -b v2.1.3 https://github.com/catppuccin/tmux.git ~/.tmux/plugins/catppuccin/tmux
 ln -s $HOME/dotfiles/nvim $HOME/.config/nvim
-
-# homebrew setup
-NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";\ 
-  echo >> /home/${USER}/.zshrc; \
-  echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/user/.zshrc; \
-  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-
-/home/linuxbrew/.linuxbrew/bin/brew install neovim node
