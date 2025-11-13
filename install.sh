a228c29d02f741f9f5806cf91d05680195faee82
Sen Wang <wangsen992@gmail.com>
2025-11-12 21:46:28 -0500

    clean up the dockerfile a bit

--- install.sh
+++ install.sh
@@ -4,13 +4,12 @@ SUDO=$(which sudo)
 
 # Pre-SETUP
 $SUDO apt update
-$SUDO apt install -y wget zsh
 
 # Make sure icons appear correctly by setting locale
 $SUDO apt update
 $SUDO apt install -y locales ncurses-term
-locale-gen en_US.UTF-8
-update-locale LANG=en_US.UTF-8
+$SUDO locale-gen en_US.UTF-8
+$SUDO update-locale LANG=en_US.UTF-8
 echo "export LANG=en_US.UTF-8"  >> ~/.zshrc
 echo "export LC_ALL=en_US.UTF-8" >> ~/.zshrc
 
@@ -24,3 +23,14 @@ mkdir -p $HOME/.config
 mkdir -p ~/.tmux/plugins/catppuccin
 git clone -b v2.1.3 https://github.com/catppuccin/tmux.git ~/.tmux/plugins/catppuccin/tmux
 ln -s $HOME/dotfiles/nvim $HOME/.config/nvim
+
+
+chsh -s $(which zsh); \
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+rm -f $HOME/.zshrc && ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";\ 
+  echo >> /home/${USER}/.zshrc; \
+  echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/user/.zshrc; \
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+/home/linuxbrew/.linuxbrew/bin/brew install neovim node
