e6f1768f6f80b775d74bddc549baa42dce42185f
Sen Wang <wangsen992@gmail.com>
2025-11-12 22:12:32 -0500

    move zsh install to front

--- install.sh
+++ install.sh
@@ -5,8 +5,12 @@ SUDO=$(which sudo)
 # Pre-SETUP
 $SUDO apt update
 
+# Install zsh
+chsh -s $(which zsh); \
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+rm -f $HOME/.zshrc && ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
+
 # Make sure icons appear correctly by setting locale
-$SUDO apt update
 $SUDO apt install -y locales ncurses-term
 $SUDO locale-gen en_US.UTF-8
 $SUDO update-locale LANG=en_US.UTF-8
@@ -24,10 +28,7 @@ mkdir -p ~/.tmux/plugins/catppuccin
 git clone -b v2.1.3 https://github.com/catppuccin/tmux.git ~/.tmux/plugins/catppuccin/tmux
 ln -s $HOME/dotfiles/nvim $HOME/.config/nvim
 
-
-chsh -s $(which zsh); \
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-rm -f $HOME/.zshrc && ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
+# homebrew setup
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";\ 
   echo >> /home/${USER}/.zshrc; \
   echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/user/.zshrc; \
